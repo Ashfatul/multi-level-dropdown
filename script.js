23d005fe65abb9ffc17a3f10e14f2c2bc9ed9469
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-05-17 19:51:05 +0600

    modal

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// loader
+
 $(function () {
    $(".loader-container").fadeOut(2000).hide(1000);
+});
+
+// Email validatior
+const emailTest = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+// modal
+
+$(".modal-container").hide();
+
+$(".display-modal").click(function () {
+   $(".modal-container").addClass("show").fadeIn(500);
+   $(".email-input").val("");
+});
+
+$(".modal-close, .modal-cancel").click(function () {
+   $(".modal-container").removeClass("show").fadeOut(500);
+});
+
+$(".modal-accept").click(function () {
+   if ($(".email-input").val().length < 1) {
+      toastr.error("Please enter a valid email");
+   } else if (emailTest.test($(".email-input").val())) {
+      toastr.success("Subscribed Successfully");
+
+      $(".email-input").val("");
+
+      $(".modal-container").hide();
+   } else {
+      toastr.error("Invalid Email");
+   }
 });
